166f0d1cf4bf605d434368312d7cd57b54c416bd
Erik Clarke <ecl@mail.med.upenn.edu>
2015-10-27 11:31:18 -0400

    pytest < 2.8 does not have warns() function

--- tests/test_argutils.py
+++ tests/test_argutils.py
@@ -1,66 +1,65 @@
 import os
-import json
 import pytest
 from collections import OrderedDict
 import argutils
 import argutils.export
 import argutils.read
 from argutils import (
-	META_KEY, DESC_KEY, EXCLUDE_FLAG,
+    META_KEY, DESC_KEY, EXCLUDE_FLAG,
 )
 
 @pytest.fixture
 def testdata_fp():
-	return os.path.join(os.path.dirname(__file__), "data")
+    return os.path.join(os.path.dirname(__file__), "data")
 
 @pytest.fixture
 def json_file(testdata_fp):
-	return os.path.join(testdata_fp, "test.json")
+    return os.path.join(testdata_fp, "test.json")
 
 @pytest.fixture
 def yaml_file(testdata_fp):
-	return os.path.join(testdata_fp, "test.yaml")
+    return os.path.join(testdata_fp, "test.yaml")
 
 @pytest.fixture
 def argsdict():
-	d = OrderedDict()
-	d[META_KEY] = {DESC_KEY: "Section description"}
-	d['arg1'] = {
-		'default': 'default_value',
-		DESC_KEY: 'Argument description/help'
-	}
-	d['arg2'] = {
-		'default': int(1),
-		DESC_KEY: 'Second argument description',
-		'type': 'int'
-	}
-	d['arg3'] = {
-		'default': int(1),
-		DESC_KEY: 'Third argument description',
-		'type': 'int',
-		EXCLUDE_FLAG: True
-	}
-	return d
+    d = OrderedDict()
+    d[META_KEY] = {DESC_KEY: "Section description"}
+    d['arg1'] = {
+        'default': 'default_value',
+        DESC_KEY: 'Argument description/help'
+    }
+    d['arg2'] = {
+        'default': int(1),
+        DESC_KEY: 'Second argument description',
+        'type': 'int'
+    }
+    d['arg3'] = {
+        'default': int(1),
+        DESC_KEY: 'Third argument description',
+        'type': 'int',
+        EXCLUDE_FLAG: True
+    }
+    return d
 
 
 def test_import_from_json(json_file, argsdict):
-	with open(json_file) as infile:
-		s = infile.read()
-		result = argutils.read.from_json(s)
-		assert isinstance(result, OrderedDict)
-		assert result == argsdict
+    with open(json_file) as infile:
+        s = infile.read()
+        result = argutils.read.from_json(s)
+        assert isinstance(result, OrderedDict)
+        assert result == argsdict
 
 def test_import_from_yaml(yaml_file, argsdict):
-	with open(yaml_file) as infile:
-		s = infile.read()
-		result = argutils.read.from_yaml(s)
-		assert isinstance(result, OrderedDict)
-		assert result == argsdict
+    with open(yaml_file) as infile:
+        s = infile.read()
+        result = argutils.read.from_yaml(s)
+        assert isinstance(result, OrderedDict)
+        assert result == argsdict
 
 
 def test_to_config(argsdict):
-	cfg_string = argutils.export.to_config(header='Section', argsdict=argsdict)
-	assert cfg_string == """## Section description
+    cfg_string = argutils.export.to_config(header='Section', argsdict=argsdict)
+    assert cfg_string == """## Section description
 [Section]
 # Argument description/help
 arg1 = default_value
@@ -69,15 +68,15 @@ arg2 = 1
 """
 
 def test_unordered_warning(argsdict):
-	"""Passing unordered argument dictionaries should raise an error."""
-	unordered_args = dict(argsdict)
-	if pytest.__version__ != "2.8.2":
-		assert False
-	with pytest.warns(UserWarning):
-		argutils.export.to_config(header='Section', argsdict=unordered_args)
+    """Passing unordered argument dictionaries should raise an error."""
+    unordered_args = dict(argsdict)
+    if pytest.__version__ != "2.8.2":
+        assert False
+    with pytest.warns(UserWarning):
+        argutils.export.to_config(header='Section', argsdict=unordered_args)
 
 def test_blank_comment():
-	blank = ""
-	assert argutils.format_comment(blank) == ""
-	assert argutils.format_comment(None) == ""
-	
+    blank = ""
+    assert argutils.format_comment(blank) == ""
+    assert argutils.format_comment(None) == ""
+    
